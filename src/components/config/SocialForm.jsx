2bdff3f214c9134eb6ff042a41b5fe9eef39c5ee
Vishal-Kamath <vishalkamath853@gmail.com>
2023-11-18 14:24:55 +0530

    ⬆️update: social media

--- src/components/config/SocialForm.jsx
+++ src/components/config/SocialForm.jsx
@@ -3,21 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineDelete } from "react-icons/ai";
 import { addSocials, clearState } from "../../Redux/slices/Config";
 import { toast } from "react-toastify";
+import { Input } from "../common/Input";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const MAX_SIZE = 1024 * 1024;
+
 const SocialForm = () => {
+  const notify = (arg) => toast(`${arg}`);
   const { message, type } = useSelector((state) => state.config);
   const dispatch = useDispatch();
   const [Title, setTitle] = useState("");
   const [base64Image, setBase64Image] = useState("");
   const [imageName, setImageName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    const name = file.name.split(".")[0];
-    const extension = file.name.split(".")[1];
+
+    if (!file) return;
+
+    const imageNameArray = file.name.split(".");
+    const extension = imageNameArray.pop();
+    const name = imageNameArray.join("");
     const date = Date.now().toString();
     const imageName = name.concat(date).concat(".").concat(extension);
-    setImageName(imageName);
+
     if (file.size > MAX_SIZE) {
       console.log(file.size);
       alert("file size exceeded");
@@ -35,6 +46,7 @@ const SocialForm = () => {
       const base64String = reader.result;
       console.log(base64Image.length);
       setBase64Image(base64String);
+      setImageName(imageName);
     };
 
     if (file) {
@@ -50,21 +62,40 @@ const SocialForm = () => {
   const createCategorySubmitHandler = (e) => {
     e.preventDefault();
 
-    if (Title.trim() !== "" && imageName.trim() !== "" && Image !== "") {
-      dispatch(
-        addSocials({
-          media: Title,
-          Image: base64Image,
-          imageName,
+    if (Title.trim() !== "" && imageName.trim() !== "") {
+      const url = `${process.env.REACT_APP_ENDPOINT}/api/v1/superadmin/config/social`;
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      setLoading(true);
+      axios
+        .post(
+          url,
+          {
+            media: Title,
+            Image: base64Image,
+            imageName,
+          },
+          {
+            headers,
+            withCredentials: true,
+          }
+        )
+        .then((res) => {
+          setLoading(false);
+          setTitle("");
+          setBase64Image("");
+          notify(res.data.message);
+          history.push("/socials");
         })
-      );
-      setTitle("");
-      setBase64Image("");
+        .catch((err) => {
+          setLoading(false);
+          notify("Something went wrong");
+        });
     }
   };
 
   useEffect(() => {
-    const notify = (arg) => toast(`${arg}`);
     if (message && type) {
       if (type === "success") {
         notify(message);
@@ -80,102 +111,99 @@ const SocialForm = () => {
   }, [dispatch, type, message]);
 
   return (
-    <div className=" flex flex-col overflow-y-scroll   h-[89vh] w-[100%] lg:w-[80%] no-scroll ">
-      <div className="w-[97%] mx-auto mt-2 mb-[1px] py-3 h-[50px] justify-center bg-white  rounded-md flex flex-col     shadow-md ">
-        <h1 className="text-black lg:text-3xl text-2xl   pl-4 ">
-          Social Media
-        </h1>
-      </div>
-      <div className="w-[97%] mx-auto  bg-white  rounded-md flex flex-col     shadow-md ">
+    <main className="w-full h-[89vh] p-5 overflow-y-auto">
+      <div className="flex flex-col max-w-lg gap-6 rounded-lg p-5 bg-white">
+        <h2 className="border-b-[1px] border-gray-300 py-2 text-lg font-semibold">
+          Add Social Media
+        </h2>
         <form
           onSubmit={createCategorySubmitHandler}
           encType="multipart/form-data"
+          className="flex flex-col gap-6"
         >
-          <div className="flex flex-col w-[95%]  mx-auto mt-5 ">
-            <label htmlFor="" className="mb-4 text-lg text-gray-400 ">
-              Media
-            </label>
-            <input
-              type="text"
-              value={Title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Title"
-              className=" h-[50px] pl-3 rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700  "
-            />
-          </div>
-          <div className="w-[95%] mx-auto flex   flex-col  lg:flex-row    ">
-            <div className="lg:w-[40%]">
-              <h2 className="text-lg font-bold mt-5  mx-4 ">Image</h2>
-              <div className=" mt-5 mx-4  flex overflow-x-scroll w-full gap-x-6 h-[430px] overflow-y-hidden  ">
-                <div className="">
-                  <div className="w-[300px] h-[350px] border-2 border-gray-200 ">
-                    <img
-                      src={base64Image}
-                      className="w-[300px] h-[350px]"
-                      alt="upload_image"
-                    />
-                  </div>
-                  <div
-                    onClick={deleteFile}
-                    className=" cursor-pointer hover:bg-slate-300 w-[300px] h-[40px] flex justify-center items-center bg-gray-200 rounded-lg mt-3 active:bg-slate-300 "
-                  >
-                    <span>{<AiOutlineDelete size={30} />}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="lg:w-[60%] lg:flex-grow lg:mt-12 ">
-              <div class="  mt-5 flex items-center justify-center   w-[95%]   lg:w-[100%] cursor-pointer ">
-                <label
-                  for="dropzone-file"
-                  class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 "
+          <div className="flex w-full flex-col gap-3">
+            <h3 className="text-lg">Image</h3>
+            {base64Image ? (
+              <>
+                <img
+                  src={base64Image}
+                  className="w-[100%] h-[200px]  lg:h-[350px] object-contain "
+                  alt="upload_image"
+                />
+                <button
+                  onClick={deleteFile}
+                  className="max-w-md mx-auto w-full gap-3 hover:bg-red-200 h-10 flex justify-center items-center bg-gray-200 rounded-lg mt-3 active:bg-slate-300 "
                 >
-                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      aria-hidden="true"
-                      class="w-10 h-10 mb-3 text-gray-400"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                      ></path>
-                    </svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span class="font-semibold">Click to upload</span> or drag
-                      and drop
-                    </p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">
-                      SVG, PNG, (MAX. 96x96px) ,MAX_SIZE=1.0MB
-                    </p>
-                  </div>
-                  <input
-                    id="dropzone-file"
-                    type="file"
-                    class="hidden"
-                    onChange={handleFileChange}
-                  />
-                </label>
-              </div>
-            </div>
+                  <span>Delete Image</span>
+                  <AiOutlineDelete className="text-[red] h-6 w-6" />
+                </button>
+              </>
+            ) : (
+              <DropZone handleFileChange={handleFileChange} />
+            )}
           </div>
-          <div className="w-full pr-4 mt-3 h-[60px] flex items-center justify-end  ">
-            <button
-              type="submit"
-              className="w-[150px] h-[45px]  bg-[#4361ee] text-white rounded-md flex items-center justify-center cursor-pointer "
-            >
-              Add
-            </button>
-          </div>
+          <Input
+            input_type="text"
+            placeholder="Social Media Link"
+            value={Title}
+            setValue={setTitle}
+          />
+
+          <button
+            type="submit"
+            className={`w-[150px] h-[45px] bg-[#4361ee] text-white rounded-md flex items-center justify-center cursor-pointer ml-auto ${
+              loading && "grayscale"
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Creating..." : "Create"}
+          </button>
         </form>
       </div>
-    </div>
+    </main>
   );
 };
 
 export default SocialForm;
+
+const DropZone = ({ handleFileChange }) => {
+  return (
+    <div className="flex cursor-pointer">
+      <label
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center h-[20rem] w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 "
+      >
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg
+            aria-hidden="true"
+            className="w-10 h-10 mb-3 text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+            ></path>
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and
+            drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            SVG, PNG, JPG or GIF (MAX SIZE 1MB)
+          </p>
+        </div>
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+      </label>
+    </div>
+  );
+};
