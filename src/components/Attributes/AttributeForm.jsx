a16d93bdfab54b7c07b29508bbc4389649e67400
Vishal-Kamath <vishalkamath853@gmail.com>
2023-11-25 17:04:45 +0530

    ⬆️update: add and edit attributes

--- src/components/Attributes/AttributeForm.jsx
+++ src/components/Attributes/AttributeForm.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../Home/Navbar";
-import Sidebar from "../Home/Sidebar";
 import { AiOutlineDelete } from "react-icons/ai";
-import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import { addVarient, clearState } from "../../Redux/slices/VarientSlice";
+import { Input } from "../common/Input";
+import { FaPlus } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const AttributeForm = () => {
+  const notify = (arg) => toast(`${arg}`);
+  const history = useHistory();
+
   const [title, setTitle] = useState("");
-  const { message, type } = useSelector((state) => state.varients);
   const [varientName, setVarientName] = useState("");
   const [arr, setArr] = useState([]);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {}, [arr]);
 
   const varientSubmitHandler = (e) => {
@@ -28,36 +31,45 @@ const AttributeForm = () => {
         arr.push(varientName.trim().toLowerCase());
         setArr(arr);
         setVarientName("");
+      } else {
+        notify("Attribute value already exists");
+        return;
       }
     }
 
-    if (title !== "" && arr.length !== 0) {
-      dispatch(
-        addVarient({
-          varientName: title,
-          value: arr,
+    if (title.trim() !== "" && arr.length !== 0) {
+      const url = `${process.env.REACT_APP_ENDPOINT}/api/v1/admin/addVarient`;
+      const headers = {
+        "Content-Type": "application/json", // You may need to include other headers based on the API requirements
+      };
+      setLoading(true);
+      axios
+        .post(
+          url,
+          {
+            varientName: title.trim(),
+            value: arr,
+          },
+          {
+            headers,
+            withCredentials: true,
+          }
+        )
+        .then((res) => {
+          setArr([]);
+          setTitle("");
+          setLoading(false);
+          notify("Attribute added successfully");
+          history.push("/attributes");
         })
-      );
-      setArr([]);
-      setTitle("");
+        .catch((err) => {
+          setLoading(false);
+          notify(err.response.data.message);
+        });
+    } else {
+      notify("Please fill all the fields");
     }
   };
-
-  useEffect(() => {
-    const notify = (arg) => toast(`${arg}`);
-    if (message && type) {
-      if (type === "success") {
-        notify(message);
-        dispatch(clearState());
-        setTimeout(() => {
-          window.location.replace("/attributes");
-        }, 1500);
-      } else {
-        notify(message);
-        dispatch(clearState());
-      }
-    }
-  }, [dispatch, type, message]);
 
   function deleteAttribute(k) {
     setArr(
@@ -79,85 +91,156 @@ const AttributeForm = () => {
         arr.push(varientName.trim().toLowerCase());
         setArr(arr);
         setVarientName("");
+      } else {
+        notify("Attribute value already exists");
       }
     }
   }
+
+  function updateAttributeValue(e, key) {
+    let newArr = [...arr];
+    newArr[key] = e.target.value;
+    setArr(newArr);
+  }
+
   return (
-    <div className="w-full ">
-      <div className="w-[97%] mx-auto mt-5  min-h-[50px] bg-white  rounded-md flex flex-col     shadow-md ">
-        <h1 className="text-xl h-[50px] flex  items-center pl-3 font-semibold ">
-          Attribute Form
-        </h1>
-      </div>
-      <div className="w-[97%] mx-auto mt-1  min-h-[300px] bg-white  rounded-md flex flex-col    shadow-md ">
-        <form onSubmit={varientSubmitHandler}>
-          <div className="flex flex-col w-[95%] h-full mx-auto mt-6 ">
-            <label htmlFor="" className="mb-4 text-lg text-gray-400 ">
-              Title
-            </label>
-            <input
+    <div className="w-full max-h-[89vh] h-full overflow-y-auto p-5">
+      <main className="w-full h-full flex flex-col gap-9 p-5 pb-12 bg-white rounded-md max-w-4xl">
+        <h2 className="text-xl font-semibold w-full border-b-2 border-gray-300 pb-3">
+          Create New Attribute
+        </h2>
+
+        <form className="flex flex-col gap-6" onSubmit={varientSubmitHandler}>
+          <div className="max-w-lg">
+            <Input
+              input_type={"text"}
+              placeholder={"Attribute name"}
               value={title}
-              type="text"
-              onChange={(e) => setTitle(e.target.value.trim().toLowerCase())}
-              className=" h-[50px] pl-3 rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700  "
-              placeholder="Title"
+              setValue={setTitle}
             />
           </div>
-          <div className="w-[95%] mx-auto h-[80px] sm:h-[40px] lg:h-[40px] px-5 mt-3 rounded-md flex items-center bg-[#bde0fe]  ">
-            To delete an attribute value , press delete button
-          </div>
-          <div className="w-[95%] flex flex-col mx-auto mt-6 ">
-            <label htmlFor="" className="mb-4 text-lg text-gray-400">
-              Attribute values
-            </label>
-            <div className="w-full h-[60px] flex flex-row items-center overflow-x-scroll ">
-              {arr.map((items, key) => {
-                return (
-                  <div className="flex mr-3 ">
-                    <input
-                      key={key}
-                      type="text"
-                      value={items}
-                      className=" min-w-[40px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 mr-1 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
-                      placeholder="value"
-                    />
-                    <span
-                      key={key}
-                      onClick={() => deleteAttribute(key)}
-                      className="flex items-center bg-slate-100 active:bg-slate-300  rounded-md w-[30px] h-[35px] "
-                    >
-                      {<AiOutlineDelete size={28} />}
-                    </span>
-                  </div>
-                );
-              })}
-              <input
-                value={varientName}
-                onChange={(e) => setVarientName(e.target.value)}
-                type="text"
-                className=" min-w-[60px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
-                placeholder="value"
-              />
-            </div>
-            <div
+
+          <h3>Attribute Values</h3>
+          <div className="flex gap-3 flex-wrap">
+            {arr.map((items, key) => {
+              return (
+                <div className="flex gap-3 border-[1px] border-gray-500 focus-within:border-black rounded-md overflow-hidden pl-3">
+                  <input
+                    key={key}
+                    type="text"
+                    value={items}
+                    className="border-none w-32 outline-none"
+                    onChange={(e) => updateAttributeValue(e, key)}
+                    placeholder="value"
+                  />
+                  <button
+                    type="button"
+                    className="text-red-600 bg-red-100 hover:bg-red-200 p-2 aspect-square rounded-sm"
+                    onClick={() => deleteAttribute(key)}
+                  >
+                    <AiOutlineDelete />
+                  </button>
+                </div>
+              );
+            })}
+            <input
+              value={varientName}
+              onChange={(e) => setVarientName(e.target.value)}
+              type="text"
+              className=" min-w-[60px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
+              placeholder="value"
+            />
+
+            <button
               onClick={() => addAttribute()}
-              className="w-[180px] h-[50px] bg-[#bde0fe] text-[#023e8a] flex items-center justify-center  rounded-md my-3 font-semibold cursor-pointer"
+              type="button"
+              className="flex gap-3 font-semibold h-9 items-center justify-center border-[1px] border-sky-600 py-2 px-4 rounded-md bg-sky-50 hover:bg-sky-200 text-sky-600"
             >
-              Add attribute value
-            </div>
-            <div className="w-full h-[60px] flex items-center justify-end  ">
-              <button
-                type="submit"
-                className="w-[150px] h-[45px] bg-[#4361ee] text-white rounded-md flex items-center justify-center cursor-pointer "
-              >
-                Save & edit
-              </button>
-            </div>
+              Add <FaPlus />
+            </button>
           </div>
+
+          <button
+            type="submit"
+            className={`rounded-md ml-auto border-[1px] border-sky-500 px-6 py-2 text-lg font-semibold hover:bg-sky-500 hover:text-white ${
+              loading ? "grayscale" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Submitting..." : "Submit"}
+          </button>
         </form>
-      </div>
+      </main>
     </div>
   );
 };
 
 export default AttributeForm;
+
+// (
+//   <div className="w-full ">
+//     <div className="w-[97%] mx-auto mt-5  min-h-[50px] bg-white  rounded-md flex flex-col     shadow-md ">
+//       <h1 className="text-xl h-[50px] flex  items-center pl-3 font-semibold ">
+//         Attribute Form
+//       </h1>
+//     </div>
+//     <div className="w-[97%] mx-auto mt-1  min-h-[300px] bg-white  rounded-md flex flex-col    shadow-md ">
+//       <form onSubmit={varientSubmitHandler}>
+//         <Input
+//           input_type={"text"}
+//           placeholder={"Attribute name"}
+//           value={title}
+//           setValue={setTitle}
+//         />
+//         <div className="w-[95%] flex flex-col mx-auto mt-6 ">
+//           <label htmlFor="" className="mb-4 text-lg text-gray-400">
+//             Attribute values
+//           </label>
+//           <div className="w-full h-[60px] flex flex-row items-center overflow-x-scroll ">
+//             {arr.map((items, key) => {
+//               return (
+//                 <div className="flex mr-3 ">
+//                   <input
+//                     key={key}
+//                     type="text"
+//                     value={items}
+//                     className=" min-w-[40px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 mr-1 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
+//                     placeholder="value"
+//                   />
+//                   <span
+//                     key={key}
+//                     onClick={() => deleteAttribute(key)}
+//                     className="flex items-center bg-slate-100 active:bg-slate-300  rounded-md w-[30px] h-[35px] "
+//                   >
+//                     {<AiOutlineDelete size={28} />}
+//                   </span>
+//                 </div>
+//               );
+//             })}
+//             <input
+//               value={varientName}
+//               onChange={(e) => setVarientName(e.target.value)}
+//               type="text"
+//               className=" min-w-[60px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
+//               placeholder="value"
+//             />
+//           </div>
+//           <div
+//             onClick={() => addAttribute()}
+//             className="w-[180px] h-[50px] bg-[#bde0fe] text-[#023e8a] flex items-center justify-center  rounded-md my-3 font-semibold cursor-pointer"
+//           >
+//             Add attribute value
+//           </div>
+//           <div className="w-full h-[60px] flex items-center justify-end  ">
+//             <button
+//               type="submit"
+//               className="w-[150px] h-[45px] bg-[#4361ee] text-white rounded-md flex items-center justify-center cursor-pointer "
+//             >
+//               Save & edit
+//             </button>
+//           </div>
+//         </div>
+//       </form>
+//     </div>
+//   </div>
+// )
--- src/components/Attributes/EditAttributeForm.jsx
+++ src/components/Attributes/EditAttributeForm.jsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../Home/Navbar";
-import Sidebar from "../Home/Sidebar";
 import { AiOutlineDelete } from "react-icons/ai";
-import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { clearState, updateVarient } from "../../Redux/slices/VarientSlice";
 import axios from "axios";
 import LoadingPage from "../common/loading";
+import { Input } from "../common/Input";
+import { FaPlus } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
 
 const EditAttributeForm = (props) => {
   const notify = (arg) => toast(`${arg}`);
+  const history = useHistory();
+
   const [title, setTitle] = useState("");
   const [id, setId] = useState("");
-  const { varients, message, type } = useSelector((state) => state.varients);
   const [varientName, setVarientName] = useState("");
   const [arr, setArr] = useState([]);
-  const [duplicateArray, setDuplicateArray] = useState([]);
   const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
 
   useEffect(() => {
     axios
@@ -52,34 +51,42 @@ const EditAttributeForm = (props) => {
         arr.push(varientName.trim().toLowerCase());
         setArr(arr);
         setVarientName("");
+      } else {
+        notify("Attribute value already exists");
+        return;
       }
     }
 
-    if (title !== "" && arr.length !== 0) {
-      dispatch(
-        updateVarient({
-          id,
-          body: {
-            varientName: title,
+    if (title.trim() !== "" && arr.length !== 0) {
+      const url = `${process.env.REACT_APP_ENDPOINT}/api/v1/admin/updateVarient?id=${id}`;
+      const headers = {
+        "Content-Type": "application/json; charset=utf-8",
+      };
+      axios
+        .put(
+          url,
+          {
+            varientName: title.trim(),
             value: arr,
           },
+          {
+            headers,
+            withCredentials: true,
+          }
+        )
+        .then((res) => {
+          setArr([]);
+          setTitle("");
+          setLoading(false);
+          notify("Attribute updated successfully");
+          history.push("/attributes");
         })
-      );
+        .catch((err) => {
+          setLoading(false);
+          notify(err.response.data.message);
+        });
     }
   };
-
-  useEffect(() => {
-    const notify = (arg) => toast(`${arg}`);
-    if (message && type) {
-      if (type === "success") {
-        notify(message);
-        dispatch(clearState());
-      } else {
-        notify(message);
-        dispatch(clearState());
-      }
-    }
-  }, [dispatch, type, message, arr, props, varients, duplicateArray]);
 
   function deleteAttribute(k) {
     setArr(
@@ -103,83 +110,81 @@ const EditAttributeForm = (props) => {
       }
     }
   }
-  return loading ? (
-    <LoadingPage />
-  ) : (
-    <div className="w-full p-5">
-      <div className="w-full mx-auto min-h-[50px] bg-white  rounded-md flex flex-col shadow-md">
-        <h1 className="text-xl h-[50px] flex  items-center pl-3 font-semibold ">
-          Attribute Form
-        </h1>
-      </div>
-      <div className="w-full mx-auto mt-1  min-h-[300px] bg-white  rounded-md flex flex-col    shadow-md ">
-        <form onSubmit={varientSubmitHandler}>
-          <div className="flex flex-col w-[95%] h-full mx-auto mt-6 ">
-            <label htmlFor="" className="mb-4 text-lg text-gray-400 ">
-              Title
-            </label>
-            <input
+
+  function updateAttributeValue(e, key) {
+    let newArr = [...arr];
+    newArr[key] = e.target.value;
+    setArr(newArr);
+  }
+
+  return (
+    <div className="w-full max-h-[89vh] h-full overflow-y-auto p-5">
+      <main className="w-full h-full flex flex-col gap-9 p-5 pb-12 bg-white rounded-md max-w-4xl">
+        <h2 className="text-xl font-semibold w-full border-b-2 border-gray-300 pb-3">
+          Edit Attribute
+        </h2>
+
+        <form className="flex flex-col gap-6" onSubmit={varientSubmitHandler}>
+          <div className="max-w-lg">
+            <Input
+              input_type={"text"}
+              placeholder={"Attribute name"}
               value={title}
-              type="text"
-              onChange={(e) => setTitle(e.target.value.trim().toLowerCase())}
-              className=" h-[50px] pl-3 rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700  "
-              placeholder="Title"
+              setValue={setTitle}
             />
           </div>
-          <div className="w-[95%] mx-auto h-[80px] sm:h-[40px] lg:h-[40px] px-5 mt-3 rounded-md flex items-center bg-[#bde0fe]  ">
-            To delete an attribute value , press delete button
-          </div>
-          <div className="w-[95%] flex flex-col mx-auto mt-6 ">
-            <label htmlFor="" className="mb-4 text-lg text-gray-400">
-              Attribute values
-            </label>
-            <div className="w-full h-[60px] flex flex-row items-center overflow-x-scroll ">
-              {arr &&
-                arr.map((items, key) => {
-                  return (
-                    <div key={key} className="flex mr-3 ">
-                      <input
-                        key={key}
-                        type="text"
-                        value={items}
-                        className=" min-w-[40px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 mr-1 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
-                        placeholder="value"
-                      />
-                      <span
-                        key={key}
-                        onClick={() => deleteAttribute(key)}
-                        className="flex items-center bg-slate-100 active:bg-slate-300  rounded-md w-[30px] h-[35px] "
-                      >
-                        {<AiOutlineDelete size={28} />}
-                      </span>
-                    </div>
-                  );
-                })}
-              <input
-                value={varientName}
-                onChange={(e) => setVarientName(e.target.value)}
-                type="text"
-                className=" min-w-[60px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
-                placeholder="value"
-              />
-            </div>
-            <div
+
+          <h3>Attribute Values</h3>
+          <div className="flex gap-3 flex-wrap">
+            {arr.map((items, key) => {
+              return (
+                <div className="flex gap-3 border-[1px] border-gray-500 focus-within:border-black rounded-md overflow-hidden pl-3">
+                  <input
+                    key={key}
+                    type="text"
+                    value={items}
+                    className="border-none w-32 outline-none"
+                    onChange={(e) => updateAttributeValue(e, key)}
+                    placeholder="value"
+                  />
+                  <button
+                    type="button"
+                    className="text-red-600 bg-red-100 hover:bg-red-200 p-2 aspect-square rounded-sm"
+                    onClick={() => deleteAttribute(key)}
+                  >
+                    <AiOutlineDelete />
+                  </button>
+                </div>
+              );
+            })}
+            <input
+              value={varientName}
+              onChange={(e) => setVarientName(e.target.value)}
+              type="text"
+              className=" min-w-[60px] h-[35px] rounded-md border text-black border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700"
+              placeholder="value"
+            />
+
+            <button
               onClick={() => addAttribute()}
-              className="w-[180px] h-[50px] bg-[#bde0fe] text-[#023e8a] flex items-center justify-center  rounded-md my-3 font-semibold cursor-pointer"
+              type="button"
+              className="flex gap-3 font-semibold h-9 items-center justify-center border-[1px] border-sky-600 py-2 px-4 rounded-md bg-sky-50 hover:bg-sky-200 text-sky-600"
             >
-              Add attribute value
-            </div>
-            <div className="w-full h-[60px] flex items-center justify-end  ">
-              <button
-                type="submit"
-                className="w-[150px] h-[45px] bg-[#4361ee] text-white rounded-md flex items-center justify-center cursor-pointer "
-              >
-                Save & edit
-              </button>
-            </div>
+              Add <FaPlus />
+            </button>
           </div>
+
+          <button
+            type="submit"
+            className={`rounded-md ml-auto border-[1px] border-sky-500 px-6 py-2 text-lg font-semibold hover:bg-sky-500 hover:text-white ${
+              loading ? "grayscale" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Updating..." : "Update"}
+          </button>
         </form>
-      </div>
+      </main>
     </div>
   );
 };
